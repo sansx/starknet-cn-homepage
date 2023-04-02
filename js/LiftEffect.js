f33f9448388b60215f2ab7ae60d41851477d4739
sansx <646924078@qq.com>
2023-04-02 14:45:53 +0800

    update

---
+++ js/LiftEffect.js
@@ -0,0 +1,87 @@
+﻿function LiftEffect(json){
+
+var array=[];
+var index = 0;
+
+for(var i =0; i<json.target.length;i++){
+	var t = $(json.target[i]).offset().top;
+	array.push(t);
+
+}
+
+function Selected(index){
+	$(json.control2).children().removeClass(json.current);
+	$(json.control2).children().eq(index).addClass(json.current);
+}
+
+$(window).on("scroll",Check);
+
+function Check(){
+	var wst = $(window).scrollTop();
+	var key =0;
+	var flag = true;
+	for(var i =0; i<array.length; i++){
+		key++;
+		if(flag){
+			if(wst >= array[array.length-key]-400){
+				index = array.length-key;
+				flag = false;
+			}else{
+				flag=true;
+			}
+		}
+	}
+	Selected(index);
+}
+
+$(json.control2).children().on("click",function(){
+
+		$(window).off("scroll");
+		index = $(this).index();
+		Selected(index);
+
+		var flag = true;
+		for(var i =0; i<array.length; i++){
+		
+			if(flag){
+				if(index == i){
+					$("html,body").stop().animate({
+						"scrollTop": array[i]-50
+					},500,function(){
+						$(window).on("scroll",Check);
+					});
+					flag = false;
+				}else{
+					flag=true;
+				}
+				
+			}
+		}
+		
+});
+
+$(".fbg-but").on("click",function(){
+		$(window).off("scroll");
+		var index = $(this).data("id");
+		Selected(index);
+		var flag = true;
+		for(var i =0; i<array.length; i++){
+			if(flag){
+				if(index == i){
+					$("html,body").stop().animate({
+						"scrollTop": array[i]-50
+					},500,function(){
+						$(window).on("scroll",Check);
+					});
+					flag = false;
+				}else{
+					flag=true;
+				}
+			}
+		}	
+});
+
+}
+
+
+
---
+++ js/index.js
@@ -0,0 +1,22 @@
+$(window).load(function(){
+$(function(){
+	var w1 = $(window).width();
+	var h1 = $(window).height();
+	if(w1>850){
+		$(".fbg-abg,.fbg-cbg").height(h1);
+		$(".fbg-bbg").height(h1-300);
+	}
+
+	$(window).resize(function(){
+   		if(w1>850){
+			window.location.reload();
+   		}
+	})
+	LiftEffect({
+		"control1": ".header", 						  
+		"control2": ".nav",                           
+		"target": [".t1",".t2",".t3",".t4"],
+		"current": "on"
+	});
+})
+})
